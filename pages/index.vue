4ba9380baf067c8ae01deb862c40b908d6a6f96b
张志超 <zzc7400@gmail.com>
2018-10-19 11:27:55 +0800

    test on sandbox
    test ok

--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,7 @@
         My scrumtrulescent Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <button class="btn">Test ok</button>
       </div>
     </div>
   </section>
